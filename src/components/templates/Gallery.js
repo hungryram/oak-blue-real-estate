9b6a5c5bddb98042a8ca402d656c1d3e9ea95df6
Brad S <94940930+brad-adrenalize@users.noreply.github.com>
2022-06-09 09:12:29 -0600

    debug gallery infinite loop

--- src/components/templates/Gallery.js
+++ src/components/templates/Gallery.js
@@ -7,32 +7,33 @@ const Gallery = ({ photos, title }) => {
 
     const [ images ] = React.useState(photos.map((photo, i) => { return {photo: photo, display: i}}))
     const [ display, setDisplay ] = React.useState(0)
-    const [ thumbnails, setThumbnails ] = React.useState(images.filter((photo => photo.display >= display && photo.display <= display + 4)))
     const [ lightbox, setLightbox ] = React.useState(false)
-
-    React.useEffect(() => {
-        if(display >= 0 && display < images.length - 4){
-            setThumbnails(images.filter((photo => photo.display >= display && photo.display <= display + 4)))
-        } 
-        else if(display >= images.length - 5){
-            const dif = images.length - display
-            const arr1 = images.slice(display, images.length)
-            let arr2 = []
-            if(dif === 4){
-                arr2 = images.slice(0,1)
-            }
-            else if(dif === 3){
-                arr2 = images.slice(0,2)
-            }
-            else if(dif === 2){
-                arr2 = images.slice(0,3)
-            }
-            else if(dif === 1){
-                arr2 = images.slice(0,4)
-            }
-            setThumbnails(arr1.concat(arr2))
+    
+        let thumbnails = []
+    if(display >= 0 && display < images.length - 4){
+        thumbnails = images.filter((photo => photo.display >= display && photo.display <= display + 4))
+    } 
+    else if(display >= images.length - 5){
+        const dif = images.length - display
+        const arr1 = images.slice(display, images.length)
+        let arr2 = []
+        if(dif === 4){
+            arr2 = images.slice(0,1)
         }
-    }, [ display, images.length, images ])
+        else if(dif === 3){
+            arr2 = images.slice(0,2)
+        }
+        else if(dif === 2){
+            arr2 = images.slice(0,3)
+        }
+        else if(dif === 1){
+            arr2 = images.slice(0,4)
+        }
+        thumbnails = arr1.concat(arr2)
+    }
+    else{
+       thumbnails =  images.filter((photo => photo.display >= display && photo.display <= display + 4))
+    }
 
     const togglePhoto = (direction) => {
         if(direction === 'next'){
@@ -52,8 +53,6 @@ const Gallery = ({ photos, title }) => {
             }
         }
     }
-
-
 
     return(
         <div className={lightbox === true ? 'lightbox' : 'galleryWrapper'}>
@@ -81,13 +80,14 @@ const Gallery = ({ photos, title }) => {
             <div className="cursor-pointer absolute bottom-4 left-1/2 -translate-x-1/2 hidden md:flex flex-row items-center justify-center py-1 px-2 bg-black/20 opacity-50 hover:opacity-100 transition-all duration-300 scale-50 translate-y-1/4 hover:bottom-9 hover:scale-100 rounded-sm">
                 {thumbnails?.map((image, i) => {
                     return(
+                        <button onClick={() => setDisplay(image.display)}>
                         <GatsbyImage 
                             key={i}
-                            onClick={() => setDisplay(image.display)}
                             image={image.photo.childImageSharp?.gatsbyImageData} 
                             alt={`Thumbnail Image for Oak Blue Real Estate Listing ${title}`}
                             className={`h-20 w-20 m-1 rounded-sm hover:scale-110 transition-all ${image.display === display ? 'shadow-lg': 'shadow-sm'}`}
                         />
+                        </button>
                     )
                 })}
             </div>
